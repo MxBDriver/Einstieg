5e59954f3adced5e8d38a6980d15020cbe89d68d
“Maximilian <maextech@gmail.com>
2021-11-09 15:38:01 +0100

    Array

---
+++ Unterricht/Array.java
@@ -0,0 +1,12 @@
+package Unterricht;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[][] var1 = { { 2, 8, 10 }, { 2, 17, 20 }, { 3, 7, 50 } };
+
+        System.out.println(var1[1][1]);
+
+    }
+    
+}
