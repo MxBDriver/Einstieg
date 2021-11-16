dcc4dd1d76e34a06c3b1b1a144c9d1db3c91a8f5
“Maximilian <maextech@gmail.com>
2021-11-16 15:41:07 +0100

    update2

--- Unterricht/Aufgabe.java
+++ Unterricht/Aufgabe.java
@@ -19,30 +19,29 @@ public class Aufgabe {
 
         // Algorithmus / Codeblock
         System.out.println("Personal Nr: " + Integer.toString(personalnummer) + " " + "Jahr: " + Integer.toString(jahr)+ " " + "Monat: " + Integer.toString(monat));
-        for (zeilen = 0; zeilen < zeiten.length;) {
+        for (zeilen = 0; zeilen <= zeiten.length;) {
             anwTag = 0;
             // keine buchung
             if (tag < zeiten[zeilen][0]) {
                 // DANN
-                System.out.println(tag + Integer.toString(-1) + "-1" + "-1" + "-1" + anwTag + "nicht anwesend");
+                System.out.println(tag + Integer.toString(-1) + "-1" + "-1" + "-1" +" "+ anwTag + " nicht anwesend");
 
             } // ENDE WENN
               // Zwei Buchungen
             else if (tag == zeiten[zeilen][0] && tag == zeiten[zeilen + 1][0]) {
                 // DANN
-                anwTag = zeiten[zeilen + 1][1] * 60 + zeiten[zeilen + 1][2]
-                        - (zeiten[zeilen][1] * 60 + zeiten[zeilen + 1][2]);
+                anwTag = zeiten[zeilen + 1][1] * 60 + zeiten[zeilen + 1][2] - (zeiten[zeilen][1] * 60 + zeiten[zeilen + 1][2]);
                 anwMonat = anwTag + anwMonat;
-                System.out.println(tag + zeiten[zeilen][1] + zeiten[zeilen][2] + zeiten[zeilen + 1][1]
-                        + zeiten[zeilen + 1][2] + anwTag);
+                System.out.println(tag +"-"+ zeiten[zeilen][1] +" "+ zeiten[zeilen][2] +" "+ zeiten[zeilen + 1][1]
+                        +" "+ zeiten[zeilen + 1][2] +" Min: "+ anwTag);
                 zeilen = zeilen + 2;
 
             } // ENDE ANSONSTEN WENN
               // Eine Buchung
             else if (tag == zeiten[zeilen][0] && tag != zeiten[zeilen + 1][0]) {
                 // DANN
-                System.out.println(tag + zeiten[zeilen][1] + zeiten[zeilen][2] + Integer.toString(-1) + "-1" + anwTag
-                        + "Buchung fehlt");
+                System.out.println(tag +"-"+ zeiten[zeilen][1] +" "+  zeiten[zeilen][2] + Integer.toString(-1) + "-1" +" "+  anwTag
+                        + " Buchung fehlt");
                 zeilen = zeilen + 1;
 
             } // ENDE ANSONSTEN WENN
